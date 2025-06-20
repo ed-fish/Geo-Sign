8d9f97da213139fc25e3fa3b7f803915ab6db510
Ed Fish <60140216+ed-fish@users.noreply.github.com>
2025-06-20 22:19:21 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,34 +2,22 @@ mt5_path = "./pretrained_weight/mt5-base"
 
 # label paths
 train_label_paths = {
-                    "CSL_News": "/mnt/fast/nobackup/scratch4weeks/ef0036/cslnews/data/train/CSL_News_Labels.json",
-                    "CSL_Daily": "./data/CSL_Daily/labels.train",
-                    "WLASL": "./data/WLASL/labels-2000.train"
+                    "CSL_News": "./data/CSL_News_Labels.json",
+                    "CSL_Daily": "./data/CSL_Daily/labels.train"
                     }
 
 dev_label_paths = {
-                    "CSL_News": "/mnt/fast/nobackup/scratch4weeks/ef0036/cslnews/data/train/CSL_News_Labels.json",
-                    "CSL_Daily": "./data/CSL_Daily/labels.dev",
-                    "WLASL": "./data/WLASL/labels-2000.dev"
+                    "CSL_News": "./data/CSL_News_Labels.json",
+                    "CSL_Daily": "./data/CSL_Daily/labels.dev"
                     }
 
 test_label_paths = {
-                    "CSL_News": "/mnt/fast/nobackup/scratch4weeks/ef0036/cslnews/data/train/CSL_News_Labels.json",
-                    "CSL_Daily": "./data/CSL_Daily/labels.test",
-                    "WLASL": "./data/WLASL/labels-2000.test"
+                    "CSL_News": "./data/CSL_News_Labels.json",
+                    "CSL_Daily": "./data/CSL_Daily/labels.test"
                     }
-
-
-# video paths
-rgb_dirs = {
-            "CSL_News": '/mnt/fast/nobackup/scratch4weeks/ef0036/cslnews/rgb_format',
-            "CSL_Daily": './dataset/CSL_Daily/sentence-crop',
-            "WLASL": "./dataset/WLASL/rgb_format"
-            }
 
 # pose paths
 pose_dirs = {
-            "CSL_News": '/mnt/fast/nobackup/scratch4weeks/ef0036/cslnews/rgb_format',
-            "CSL_Daily": '/mnt/fast/nobackup/scratch4weeks/ef0036/csl_daily/pose_format',
-            "WLASL": "/mnt/fast/nobackup/scratch4weeks/ef0036/wlasl"
+            "CSL_News": './data/cslnews/pose_format',
+            "CSL_Daily": '/data/csldaily'
             }
